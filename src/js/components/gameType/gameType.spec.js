5695256a3ed79d499b6b75de48435fe2eaa200f6
anas youbi <anasyoubi@gmail.com>
2018-08-20 12:18:06 +0200

    Update gameType.spec.js

--- src/js/components/gameType/gameType.spec.js
+++ src/js/components/gameType/gameType.spec.js
@@ -1,13 +1,20 @@
 import GameType from './'
 
+const element = {
+  querySelectorAll: jest.fn().mockImplementation(() => {
+    return []
+  })
+}
+
 describe('testing GameTypeComponent', () => {
   test('having Element attribute', () => {
-    const gameType = new GameType({})
+    const gameType = new GameType({element})
     expect(gameType.hasOwnProperty('element')).toBeTruthy()
   })
   test('having Element contain our html code', () => {
-    const gameType = new GameType({})
+    const gameType = new GameType({element})
     gameType.render()
     expect(gameType.element.innerHTML).toEqual(expect.stringContaining('gameType-container'))
+    expect(element.querySelectorAll).toHaveBeenCalledTimes(1);
   })
 })
