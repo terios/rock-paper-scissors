d9c6bc860e98d20b966cb41766ed73f48da78df0
anas youbi <anasyoubi@gmail.com>
2018-08-20 12:15:09 +0200

    Update player.spec.js

--- src/js/components/players/player.spec.js
+++ src/js/components/players/player.spec.js
@@ -1,13 +1,20 @@
 import Player from './'
 
+const element = {
+  querySelectorAll: jest.fn().mockImplementation(() => {
+    return []
+  })
+}
+
 describe('testing Player Component', () => {
   test('having Element attribute', () => {
     const player = new Player({})
     expect(player.hasOwnProperty('element')).toBeTruthy()
   })
   test('having Element contain our html code', () => {
-    const player = new Player({})
+    const player = new Player({element})
     player.render()
     expect(player.element.innerHTML).toEqual(expect.stringContaining('player'))
+    expect(element.querySelectorAll).toHaveBeenCalledTimes(1);
   })
 })
